6ae044c5cdf98f96ce716b3008cf6be09c2797bd
toast <levirooth29@gmail.com>
2025-04-04 10:04:40 +0200

    test

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ function closeModal() {
     const modal = document.getElementById('modal');
     modal.classList.remove('show');
 }
+
+// Function to hide the logo after 1.5 seconds
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('logo-container').style.opacity = '0';
+    }, 1500);
+};
