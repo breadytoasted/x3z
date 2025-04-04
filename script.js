cf971e8f5142d471ad3c92d500cfba1e20b36794
toast <levirooth29@gmail.com>
2025-04-04 11:22:45 +0200

    buerp

--- script.js
+++ script.js
@@ -4,37 +4,34 @@ const modalImage = document.getElementById('modal-image');
 const modalText = document.getElementById('modal-text');
 const closeModal = document.getElementById('close-modal');
 
+let currentLink = ""; // Store the link of the clicked image
+
 // Event listener for each image
 images.forEach(image => {
-    let isImageClicked = false;
-
-    // When an image is clicked
     image.addEventListener('click', function () {
-        // If the image has not been clicked before, open the modal
-        if (!isImageClicked) {
-            modalImage.src = image.src;  // Set the modal image to the clicked image
-            modalText.textContent = image.getAttribute('data-text');  // Set the text in the modal
-            modal.classList.add('show');  // Show the modal
-            isImageClicked = true;  // Mark the image as clicked to show text
-        } else {
-            // If the image has already been clicked, redirect to the link
-            window.location.href = image.getAttribute('data-link');
-        }
+        // Open modal
+        modalImage.src = image.src;  
+        modalText.textContent = image.getAttribute('data-text');  
+        modal.classList.add('show');  
+        currentLink = image.getAttribute('data-link'); // Store the link
     });
 });
 
-modal.addEventListener('click', function (e) {
-    if (e.target === modal) {
-        modal.classList.remove('show');
-        images.forEach(image => {
-            image.isImageClicked = false; // Reset state
-        });
+// Clicking the modal image redirects
+modalImage.addEventListener('click', function () {
+    if (currentLink) {
+        window.location.href = currentLink;
     }
 });
 
+// Close the modal when clicking outside modal content
+modal.addEventListener('click', function (e) {
+    if (e.target === modal) {
+        modal.classList.remove('show');
+    }
+});
+
+// Close the modal when clicking the close button
 closeModal.addEventListener('click', function () {
     modal.classList.remove('show');
-    images.forEach(image => {
-        image.isImageClicked = false; // Reset state
-    });
 });
