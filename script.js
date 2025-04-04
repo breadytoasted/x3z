eaded1a9bb6a627d2adc786bf31b85c83b9aa43a
toast <levirooth29@gmail.com>
2025-04-04 11:16:00 +0200

    test

--- script.js
+++ script.js
@@ -4,27 +4,41 @@ const modalImage = document.getElementById('modal-image');
 const modalText = document.getElementById('modal-text');
 const closeModal = document.getElementById('close-modal');
 
+// Event listener for each image
 images.forEach(image => {
     let isImageClicked = false;
 
+    // When an image is clicked
     image.addEventListener('click', function () {
+        // If the image has not been clicked before, open the modal
         if (!isImageClicked) {
-            modalImage.src = image.src;
-            modalText.textContent = image.getAttribute('data-text');
-            modal.classList.add('show');
-            isImageClicked = true;
+            modalImage.src = image.src;  // Set the modal image to the clicked image
+            modalText.textContent = image.getAttribute('data-text');  // Set the text in the modal
+            modal.classList.add('show');  // Show the modal
+            isImageClicked = true;  // Mark the image as clicked to show text
         } else {
+            // If the image has already been clicked, redirect to the link
             window.location.href = image.getAttribute('data-link');
         }
     });
 });
 
+// Close the modal if you click outside the modal content area
 modal.addEventListener('click', function (e) {
     if (e.target === modal) {
-        modal.classList.remove('show');
+        modal.classList.remove('show');  // Hide the modal
+        images.forEach(image => {
+            // Reset the image clicked status when modal is closed
+            image.isImageClicked = false;
+        });
     }
 });
 
+// Close the modal when the close button is clicked
 closeModal.addEventListener('click', function () {
-    modal.classList.remove('show');
+    modal.classList.remove('show');  // Hide the modal
+    images.forEach(image => {
+        // Reset the image clicked status when modal is closed
+        image.isImageClicked = false;
+    });
 });
