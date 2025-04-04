aea1e4d86e7cfee0f7a495a4703a82e2a4134346
toast <levirooth29@gmail.com>
2025-04-04 11:24:42 +0200

    ok

--- script.js
+++ script.js
@@ -6,14 +6,24 @@ const closeModal = document.getElementById('close-modal');
 
 let currentLink = ""; // Store the link of the clicked image
 
+// Create click-to-open text dynamically
+const clickToOpen = document.createElement('p');
+clickToOpen.textContent = "Click image to visit link";
+clickToOpen.style.color = "#bbb";
+clickToOpen.style.fontSize = "16px";
+clickToOpen.style.marginTop = "10px";
+clickToOpen.style.opacity = "0.7";
+
+modalText.insertAdjacentElement('afterend', clickToOpen);
+
 // Event listener for each image
 images.forEach(image => {
     image.addEventListener('click', function () {
-        // Open modal
         modalImage.src = image.src;  
         modalText.textContent = image.getAttribute('data-text');  
         modal.classList.add('show');  
-        currentLink = image.getAttribute('data-link'); // Store the link
+        currentLink = image.getAttribute('data-link'); 
+        clickToOpen.style.display = "block"; // Show text when modal opens
     });
 });
 
