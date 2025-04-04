02101682b69aca5253bf7a2be809f60732c3a8f4
toast <levirooth29@gmail.com>
2025-04-04 10:19:04 +0200

    test

--- script.js
+++ script.js
@@ -13,10 +13,3 @@ function closeModal() {
     const modal = document.getElementById('modal');
     modal.classList.remove('show');
 }
-
-// Function to hide the logo after the animation ends (around 7 seconds)
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById('logo-container').style.opacity = '0';
-    }, 7000); // Hides logo after 7 seconds
-};
